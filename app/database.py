9d2d60eada84a80381eae1c2b232851517c74be7
amirreza.rezaie <82rezaeei@gmail.com>
2026-05-17 18:43:58 +0330

    run pipline

--- app/database.py
+++ app/database.py
@@ -1,4 +1,4 @@
-"""Database configuration and connection."""
+"""Database configuration and connection,."""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
